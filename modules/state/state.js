e95937269c7c8eccd1836ddc7b1e01c15a88c235
Nicholas Jitkoff <nicholas@jitkoff.com>
2013-01-15 18:33:19 -0800

    Remove header, add keypath logic

--- modules/state/state.js
+++ modules/state/state.js
@@ -1,23 +1,6 @@
-/*
-Copyright 2012 Google Inc. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
-var values = {};
 function State() {
   this.values = {};
 };
@@ -27,12 +10,31 @@ State.prototype.allValues = function () {
 	return this.values; 
 }
 
-State.prototype.addValues = function(newValues) {
-	for (var attrname in newValues) {
-    //console.log(attrname + " set to " + newValues[attrname]);
-		this.values[attrname] = newValues[attrname];
-	}
-	this.emit("StateEvent", newValues);
+function setValueForKeyPath(object, value, keypath) {
+  // All keys are treated as keypaths, allowing sub-element
+  // modification for highly noisy sources
+  var components = keypath.split(".");
+  var parent = object;
+  var component;
+  while (component = components.shift()) {      
+    if (components.length) {
+      if (!parent[component]) parent[component] = {};
+      parent = parent[component];
+    } else if (value != undefined) {
+      parent[component] = value;
+    } else {
+      delete parent[component];
+    }
+  }
+}
+
+State.prototype.addValues = function(values) {
+  for (var keypath in values) {
+    var value = values[keypath];
+    setValueForKeyPath(this.values, value, keypath);
+    //this.values[keypath] = value;
+  }
+  this.emit("StateEvent", values);
 }
 
 exports.State = State;

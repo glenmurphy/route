036efd14c626193ec3bba308e94684b31b7301dd
Nicholas Jitkoff <nicholas@jitkoff.com>
2013-06-02 14:31:24 -0700

    Room: module added

---
+++ modules/room/room.js
@@ -0,0 +1,22 @@
+var EventEmitter = require('events').EventEmitter;
+var http = require('http');
+var util = require('util');
+
+function Room(data, name) {
+  this.id = name;
+  this.name = data.label || name;
+  this._private = function () {};
+  this.commands = data.commands;
+  for (var key in data.values) { this[key] = data.values[key];}
+  // Rooms have a private dictionary for attributes that may be cyclical
+  Object.defineProperty( this, '_private', {value: {}, writable:true, configurable:true, enumerable:false});
+} 
+util.inherits(Room, EventEmitter);
+
+Room.prototype.exec = function(command, params) {
+  var value = this.commands[command];
+  console.log("executing room event", command, value)
+  if (value) this.route.execCommands(value, params, command);    
+};
+
+exports.Room = Room;

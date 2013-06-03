390762ae1e110fd23607439cfdd2129932be5343
Nicholas Jitkoff <nicholas@jitkoff.com>
2013-06-02 19:12:39 -0700

    Pebble, added client names, response.

--- modules/pebble/pebble.js
+++ modules/pebble/pebble.js
@@ -14,25 +14,35 @@ var http = require('http');
 
 function Pebble(data) {
   this.port = data.port || 9002
+  this.ids = data.ids;
   this.server = http.createServer(this.httpReq.bind(this)).listen(this.port);
 };
 util.inherits(Pebble, EventEmitter);
 
 
 Pebble.prototype.httpReq = function(req, res) {
-  var info = url.parse(req.url, true);
+  var id = req.headers['x-pebble-id'];
+  var device = this.ids[id];
+  if (device) {
+    var info = url.parse(req.url, true);
+    var action = info.path.substring(1);
 
-  if (req.method == "POST") {
-    req.setEncoding('utf8');
-    var data = "";
-    req.on('data', function(chunk) { data += chunk; });
-    req.on('end', function(chunk) {
-
-      this.emit("DeviceEvent", "Button", JSON.parse(data));
-
-      res.writeHead(200);
-      res.end();
-    }.bind(this));
+    if (req.method == "POST") {
+      req.setEncoding('utf8');
+      req.data = "";
+      req.on('data', function(chunk) { req.data += chunk; });
+      req.on('end', function() {
+        this.emit("DeviceEvent", device + "." + action, JSON.parse(req.data));
+        var resData = {1 : "Success"};
+        res.writeHead(200);
+        res.write(JSON.stringify(resData));
+        console.log(JSON.stringify(resData));
+        res.end();
+      }.bind(this));
+    }
+  } else {
+    res.writeHead(200);
+    res.end();
   }
 };
 

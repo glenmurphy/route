6650aced8d0b8fb46c6f43ad174c3ad92171f841
andywarr <andywarr@gmail.com>
2013-07-23 23:05:50 -0700

    Updated leap module to emit gestures

--- modules/leap/leap.js
+++ modules/leap/leap.js
@@ -3,16 +3,22 @@ var Leapjs = require('leapjs');
 var util = require('util');
 
 function Leap(data) {
-  this.controller = new Leapjs.Controller(
-    {host: data.host,
+  this.threshold = data.threshold || 10;
+
+  var controllerOptions = {host: data.host,
     port: data.port || '127.0.0.1',
     enableGestures: data.enableGestures || false,
     enableHeartbeat: data.enableHeartbeat || true,
     heartbeatInterval: data.heartbeatInterval || 100,
     requestProtocolVersion: data.requestProtocolVersion || 3
-  });
+  };
+  this.controller = new Leapjs.Controller(controllerOptions);
 
   this.controller.on('frame', this.handleFrame.bind(this));
+
+  if (controllerOptions.enableGestures) {
+    this.controller.on('gesture', this.handleGesture.bind(this));
+  }
 
   this.controller.on('ready', function() {
     console.log("ready");
@@ -41,9 +47,25 @@ function Leap(data) {
 }
 util.inherits(Leap, EventEmitter);
 
+Leap.frameCount = 0;
+
 Leap.prototype.handleFrame = function(frame) {
   // console.log(frame);
-  this.emit("DeviceEvent", frame);
+  // this.emit("DeviceEvent", frame);
+};
+
+Leap.prototype.handleGesture = function(gesture, frame) {
+
+  if (gesture.state == "start") {
+    Leap.frameCount = 0;
+  }
+  else if (gesture.state == "update") {
+    Leap.frameCount++;
+  }
+  else if (gesture.state == "stop" && Leap.frameCount >= this.threshold) {
+    console.log(gesture.type);
+    this.emit("DeviceEvent", gesture.type);
+  }
 };
 
 exports.Leap = Leap;

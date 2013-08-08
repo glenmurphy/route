d2fd61492f0e7f108e5d93639a60f2cc91208406
Nicholas Jitkoff <nicholas@jitkoff.com>
2013-08-07 21:48:56 -0700

    Voice: support sending phrases to devices.

--- modules/voice/voice.js
+++ modules/voice/voice.js
@@ -18,13 +18,14 @@ function Voice(data) {
   this.commandGuardPhrase = data.commandGuardPhrase;
   this.listeningTimeout = null;
   this.devices = data.devices;
-  this.timeoutDuration = data.timeoutDuration || 10000;
+  this.timeoutDuration = data.timeoutDuration || 20000;
   this.mappings = [];
   this.synonyms = data.synonyms;
   this.route = data.route;
   this.port = data.port || 9001
   this.server = http.createServer(this.httpReq.bind(this)).listen(this.port);
   this.lastEvent = null;
+  this.lastEventTime = new Date();
   this.openEndedPhrases = [];
 
   for (var phrase in this.phrases) {
@@ -39,17 +40,24 @@ function Voice(data) {
 util.inherits(Voice, EventEmitter);
 
 
-Voice.prototype.httpReq = function(req, res) {
+Voice.prototype.httpReq = function(req, res) { 
   var info = url.parse(req.url, true);
+  var query = info.query;
   res.writeHead(200);
+  console.log(info.path);
   if (info.path == "/phrases") {
+    if (query.device) console.log("*  Voice: Sending phrases to", query.device);
+    var context = this.contextForDevice(query.device);
     res.write(this.phrases.join("\n"));    
   } else {
-    this.handleVoiceInput(info.query);
+    this.handleVoiceInput(query);
   }
   res.end();
 }; 
 
+Voice.prototype.contextForDevice = function(device) { 
+  return this.devices[device] || device;
+}
 
 Voice.prototype.addMapping = function(mapping) {
   this.mappings.push(mapping);
@@ -85,16 +93,13 @@ Voice.prototype.handleVoiceInput = function(params) {
   var isPartial = (params.action == "partial");
   if (isPartial) return;
 
-  if (params.action == "completed") {
-    this.stoppedListening(params);
-  }
-
   var strings = params.string;
   if (typeof strings === 'string') strings = [strings];
   var scores = params.score;
   if (typeof strings === 'string') scores = [scores];
 
   if (!strings) return;
+  console.log("*  Voice: ", strings);
 
   // go through each possible voice string (usually only one)
   // and break after the first succeeds
@@ -110,7 +115,6 @@ Voice.prototype.handleVoiceInput = function(params) {
     var originalString = string;
     var result = this.normalizeString(string);
     var resultParams = result.params;
-
     string = result.string;
     if (context) {
       string = context + "." + string;
@@ -118,18 +122,25 @@ Voice.prototype.handleVoiceInput = function(params) {
     }
 
     var events = this.route.allEventsMatchingName("Voice." + string);
-    console.log("Voice." + string + ":", events);
+    if (this.debug) console.log("Voice." + string + ":", events);
     var event = events.shift();
     if (event) {
-      if (this.lastEvent != string) { // Ignore repeated events within one second (handle multiple listeners)
-        this.emit("DeviceEvent", string, resultParams);
+
+      var now = new Date();
+      var timeSinceLastEvent = now.getTime() - this.lastEventTime.getTime();
+      console.log("elapsed", timeSinceLastEvent);
+      if (timeSinceLastEvent < 3000) {
+        console.log("ignore duplicate event", string);
+      } else if (this.lastEvent != string) { // Ignore repeated events within one second (handle multiple listeners)
         this.lastEvent = string;
         resultParams.recognizedString = originalString;
+        this.emit("DeviceEvent", string, resultParams);
         if (this.lastEventTimeout) clearTimeout(this.lastEventTimeout);
         this.lastEventTimeout = setTimeout(function (){ this.lastEvent = null }.bind(this), 1000);
       } else {
         console.log('ignoring duplicate event', string);
       }
+      this.lastEventTime = now; // Maybe earlier?
       matched = true;
       break;
     } 
@@ -147,14 +158,20 @@ Voice.prototype.handleVoiceInput = function(params) {
 
   this.emit("StateEvent", {lastVoiceString: resultParams.recognizedString});
 
-  // Any input triggers stoppedListening
-  this.stoppedListening(params);
+  if (params.action == "completed") {
+    this.stoppedListening(params);
+  }
 }
 
 Voice.prototype.normalizeString = function(string) {
   var splitWords = ["to", "for"];
   var params = {};
 
+  if (this.synonyms && this.synonyms[string]) {
+    string = this.synonyms[string] // Resolve a synonym
+    console.log("*  Voice: using synonym: " + string);
+  }
+  
   for (var prefix in this.openEndedPhrases) {
     var prefixString = this.openEndedPhrases[prefix];
     var index = string.indexOf(prefixString);
@@ -178,7 +195,7 @@ Voice.prototype.normalizeString = function(string) {
 
   if (this.synonyms && this.synonyms[string]) {
     string = this.synonyms[string] // Resolve a synonym
-    console.log("Using: " + string);
+    console.log("*  Voice: using synonym: " + string);
   }
   string = string.camelcase();
   return {string:string, params:params};

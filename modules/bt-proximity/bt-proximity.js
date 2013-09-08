9f3ce7742f6004d9b1f1bfda83386e6e0a3949bc
Glen Murphy <glen@glenmurphy.com>
2013-09-08 16:37:43 -0700

    Update BTProximity
    Adds dependencies on Noble, removes deps on command-line tools.

--- modules/bt-proximity/bt-proximity.js
+++ modules/bt-proximity/bt-proximity.js
@@ -1,44 +1,34 @@
-var spawn = require('child_process').spawn,
-	  util = require('util'),
+var noble = require('noble'),
+    util = require('util'),
     EventEmitter = require('events').EventEmitter;
 
-/**
- * This is a pile of hacks; it uses the BlueZ command line tools to 
- * look for a specific bluetooth device. Because those tools can be
- * somewhat brittle (or maybe it's just my adapter), it restarts the
- * bluetooth device each time.
- *
- * Before using, you need to do
- *   hcitool lescan
- *   hcitool lecc [MACADDRESS]
- */
 function BTProximity(data) {
   this.mac = data.mac;
   this.name = data.name;
-  this.init();
+
+  noble.on('stateChange', this.handleStateChange.bind(this));
+  noble.on('discover', this.handleDiscover.bind(this));
 
   setInterval(this.checkAway.bind(this), 5000);
-  process.on('SIGINT', this.shutdown.bind(this));
 }
 util.inherits(BTProximity, EventEmitter);
 
-BTProximity.SCANPRESENT = 40000; // how often we SCAN when present.
-BTProximity.SCANAWAY = 5000; // how often we SCAN when away.
 BTProximity.AWAYAFTERTIME = 90000; // how long we can go without seeing a device before we consider you away
+
+BTProximity.prototype.handleStateChange = function(state) {
+  console.log(state);
+  if (state === "poweredOn") {
+    console.log("BTProximity: Powered on");
+    this.init();
+  }
+};
 
 BTProximity.prototype.exec = function(command, data) {
 };
 
 BTProximity.prototype.init = function() {
-  if (this.tool)
-    this.tool.kill();
-
-  this.tool = spawn('gatttool', ['-b', this.mac, '-I'], {detached: true});
-  this.tool.stdout.on("data", this.handleData.bind(this));
-  this.tool.stderr.on("data", this.handleData.bind(this));
-  this.tool.on("close", this.handleClose.bind(this));
-
-  this.setScanRate(BTProximity.SCANPRESENT);
+  console.log("Initing...");
+  noble.startScanning([], true);
   if (!this.lastSeen) {
     this.lastSeen = new Date().getTime(); // Pretend we're here.
     this.present = true;
@@ -46,35 +36,12 @@ BTProximity.prototype.init = function() {
 };
 
 BTProximity.prototype.shutdown = function() {
-  console.log("BTProximity: Shutting down gatttool...");
-  if (this.tool)
-    this.tool.kill();
-  process.exit();
-};
-
-BTProximity.prototype.handleClose = function() {
-  console.log("BTProximity: gatttool closed");
-  this.tool = null;
-  setTimeout(this.init.bind(this), 5000);
 };
 
 BTProximity.prototype.checkAway = function() {
-  var time = new Date().getTime();
-  if (this.lastSeen + BTProximity.AWAYAFTERTIME < time) {
+  if (this.lastSeen + BTProximity.AWAYAFTERTIME < new Date().getTime()) {
     this.setAway();
   }
-};
-
-BTProximity.prototype.setScanRate = function(rate) {
-  if (this.scanInterval)
-    clearInterval(this.scanInterval);
-  this.scanInterval = setInterval(this.scan.bind(this), rate);
-};
-
-BTProximity.prototype.scan = function () {
-  if (!this.tool) return;
-  console.log("BTProximity: Scanning: " + (new Date()).toLocaleTimeString());
-  this.tool.stdin.write('connect\n');
 };
 
 BTProximity.prototype.setAway = function() {
@@ -83,7 +50,6 @@ BTProximity.prototype.setAway = function() {
   this.present = false;
   console.log("BTProximity: User is away " + (new Date()).toLocaleTimeString());
   this.emit("DeviceEvent", "Away");
-  this.setScanRate(BTProximity.SCANAWAY);
 };
 
 BTProximity.prototype.setPresent = function() {
@@ -93,19 +59,13 @@ BTProximity.prototype.setPresent = function() {
   this.present = true;
   console.log("BTProximity: User found " + (new Date()).toLocaleTimeString());
   this.emit("DeviceEvent", "Present");
-  this.setScanRate(BTProximity.SCANPRESENT);
 };
 
-BTProximity.prototype.handleData = function(data) {
-  var lines = data.toString().split("\n");
-  for (var i = 0; i < lines.length; i++) {
-    line = lines[i].toLowerCase();
-    if (line.indexOf("connection successful") != -1 || line.indexOf("[con]") != -1) {
-      console.log("BTProximity: Found: " + (new Date()).toLocaleTimeString());
-      this.setPresent();
-    } else if (line.indexOf("too many open files") != -1) {
-      this.init();
-    }
+BTProximity.prototype.handleDiscover = function(peripheral) {
+  // Convert UUID to MAC
+  var mac = peripheral.uuid.toUpperCase().replace(/(.{2})(?=.)/g,"$1:");
+  if (mac == this.mac) {
+    this.setPresent();
   }
 };
 

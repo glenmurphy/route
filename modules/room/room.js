072e2f7ba08e47cdbdb766bdbc161302b391773e
Nicholas Jitkoff <nicholas@jitkoff.com>
2014-02-03 17:32:32 -0800

    Room: setter that emits state events

--- modules/room/room.js
+++ modules/room/room.js
@@ -7,6 +7,7 @@ function Room(data, name) {
   this.name = data.label || name;
   this._private = function () {};
   this.commands = data.commands;
+  this.activities = data.activities;
   for (var key in data.values) { this[key] = data.values[key];}
   // Rooms have a private dictionary for attributes that may be cyclical
   Object.defineProperty( this, '_private', {value: {}, writable:true, configurable:true, enumerable:false});
@@ -17,5 +18,12 @@ Room.prototype.exec = function(command, params) {
   var value = this.commands[command];
   if (value) this.route.execCommands(value, params, command);    
 };
+
+Room.prototype.set = function(key, value) {
+  var state = {};
+  state[this.id + "." + key] = value;
+  this[key] = value;
+  this.emit("StateEvent", state);
+}
 
 exports.Room = Room;

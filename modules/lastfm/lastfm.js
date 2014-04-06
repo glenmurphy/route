f6d2df8c87fbc3bcb357815b14e81ef747197600
Andy <andywarr@Eva.local>
2014-04-06 10:07:34 -0700

    Added LastFM module

---
+++ modules/lastfm/lastfm.js
@@ -0,0 +1,39 @@
+var EventEmitter = require('events').EventEmitter;
+var LastFmNode = require('lastfm').LastFmNode;
+var util = require('util');
+
+function LastFM(data) {
+	this.key = data.key;
+  this.secret = data.secret;
+  this.username = data.username;
+
+  this.lastfm = new LastFmNode({api_key: this.key, secret: this.secret});
+  this.trackStream = this.lastfm.stream(this.username);
+
+  this.trackStream.on('nowPlaying', this.nowPlaying.bind(this));
+  this.trackStream.on('stoppedPlaying', this.stoppedPlaying.bind(this));
+
+  this.trackStream.start();
+
+  this.lastPlayedTrack;
+}
+util.inherits(LastFM, EventEmitter);
+
+LastFM.prototype.nowPlaying = function(track) {
+  var currentDate = new Date();
+
+  this.lastPlayedTrack = {
+    album: track.album['#text'],
+    artist: track.artist['#text'],
+    played: currentDate.getTime(),
+    track: track.name
+  };
+
+  this.emit("DeviceEvent", "NowPlaying", track);
+};
+
+LastFM.prototype.stoppedPlaying = function(track) {
+  this.emit("DeviceEvent", "StoppedPlaying", track);
+};
+
+exports.LastFM = LastFM;

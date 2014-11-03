f2344888c9ea3af908e7c96fa8f5e9282923b5de
Nicholas Jitkoff <nicholas@jitkoff.com>
2014-11-02 17:21:27 -0800

    Powermate: added press-flicks

--- modules/powermate/powermate.js
+++ modules/powermate/powermate.js
@@ -57,40 +57,55 @@ PowerMateKnob.prototype.handleEvent = function(event, value) {
 }
 
 PowerMateKnob.prototype.knobTurned = function(delta) {
-  this.totalDelta += Math.abs(delta);
-  if (this.totalDelta > 2) { // Ignore a small amount of turning
+ 
+  if (this.pressed) {
+    this.totalDelta += parseInt(delta);
+    this.totalDistance += Math.abs(delta);
+   }
+
+  if (this.totalDistance > 2) { // Ignore a small amount of turning
     this.turned = true;
     clearTimeout(this.holdTimeout);
   }
+
+  if (!this.flicked && Math.abs(this.totalDelta) > 10) {
+    this.emit("DeviceEvent", this.pressed ? "KnobPressFlicked" : "KnobFlicked" , {context: this.context, value: delta, totalDelta: this.totalDelta});
+    this.flicked = true;
+  }
+
   console.log(this.id, delta);
-  this.emit("DeviceEvent", this.pressed ? "PushTurned" : "Turned" , {context: this.context, value: delta, totalDelta: this.totalDelta});
+  this.emit("DeviceEvent", this.pressed ? "KnobPressTurned" : "KnobTurned" , {context: this.context, value: delta, totalDelta: this.totalDelta});
 }
 
 PowerMateKnob.prototype.knobDown = function() {
   this.pressed = true;
   this.turned = false;
   this.held = false;
+  this.flicked = false
   this.totalDelta = 0;
+  this.totalDistance = 0;
   this.holdTimeout = setTimeout(this.knobLongPressed.bind(this), 600);
 
-  this.emit("DeviceEvent", "ButtonDown", {context: this.context});
+  this.emit("DeviceEvent", "KnobDown", {context: this.context});
 }
 
 PowerMateKnob.prototype.knobUp = function() {
+  this.totalDelta = 0;
+  this.totalDistance = 0;
   this.pressed = false;
   clearTimeout(this.holdTimeout);
 
   if (!this.turned && !this.held) this.knobPressed();
-  this.emit("DeviceEvent", "ButtonUp", {context: this.context});
+  this.emit("DeviceEvent", "KnobUp", {context: this.context});
 }
 
 PowerMateKnob.prototype.knobPressed = function() {
-  this.emit("DeviceEvent", "ButtonPressed", {context: this.context});
+  this.emit("DeviceEvent", "KnobPressed", {context: this.context});
 }
 
 PowerMateKnob.prototype.knobLongPressed = function() {
   this.held = true;
-  this.emit("DeviceEvent", "ButtonLongPressed", {context: this.context});
+  this.emit("DeviceEvent", "KnobLongPressed", {context: this.context});
 }
 
 
